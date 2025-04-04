5ee932115eb7068b3c12c7297a77eab4a4f771b1
Or Zilberberg <orzilbe@gmail.com>
2025-04-04 17:06:08 +0300

    Delete src directory

--- src/components/ErrorMessage.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-const ErrorMessage = ({ message }) =>
-    message ? <p className="text-red-500 text-sm">{message}</p> : null;
-export default ErrorMessage;
--- src/components/FormContainer.tsx
+++
@@ -1,7 +0,0 @@
-
-const FormContainer = ({ children }) => (
-    <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-      {children}
-    </div>
-  );
-  export default FormContainer;
