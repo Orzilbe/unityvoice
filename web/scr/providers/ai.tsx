621e7007db45b52237fbe539bbdb268b70fb52b7
Or Zilberberg <orzilbe@gmail.com>
2025-04-05 10:56:41 +0300

    Delete web directory

--- web/scr/providers/ai.tsx
+++
@@ -1,9 +0,0 @@
-import {ReactNode} from "react";
-import { createAI } from 'ai/rsc';
-
-
-export const AIContext = createAI<any[], ReactNode[]>({
-  initialUIState: [],
-  initialAIState: [],
-  actions: { submitUserMessage },
-});
